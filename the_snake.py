64c9f95147c0b3fd5dd21161ded4c141e275e483
Дмитрий Силачёв <dsilachev@MacBook-Air-Dmitrij.local>
2025-03-11 13:23:13 +0300

    Changes after second review

--- the_snake.py
+++ the_snake.py
@@ -2,11 +2,13 @@ import random
 
 import pygame as pg
 
+
 # Константы для размеров поля и сетки:
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
+
 
 # Направления движения:
 UP = (0, -1)
@@ -14,29 +16,38 @@ DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
+
 # Цвет фона - черный:
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
 
-# Цвет границы ячейки
+
+# Цвет границы ячейки:
 BORDER_COLOR = (93, 216, 228)
 
-# Цвет яблока
+
+# Цвет яблока:
 APPLE_COLOR = (255, 0, 0)
 
-# Цвет змейки
+
+# Цвет змейки:
 SNAKE_COLOR = (0, 255, 0)
+
 
 # Скорость движения змейки:
 SPEED = 20
 
+
 # Позиция:
 POSITION = (0, 0)
+
 
 # Настройка игрового окна:
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 
+
 # Заголовок окна игрового поля:
 pg.display.set_caption('Змейка')
+
 
 # Настройка времени:
 clock = pg.time.Clock()
@@ -52,18 +63,16 @@ class GameObject:
 
     def draw(self):
         """Метод-заглушка для наследников."""
-        raise NotImplementedError(
-            'Метод draw будет переопределён в дочерних классах'
-        )
+        raise NotImplementedError('Метод draw будет переопределён в дочерних классах')
 
 
 class Apple(GameObject):
-    """Класс для яблока, наследуется от GameObject."""
+    """Класс для яблока."""
 
     def __init__(self, body_color=APPLE_COLOR):
-        """Инициализирует яблоко с позицией и цветом."""
+        """Инициализирует яблоко."""
         super().__init__(body_color=body_color)
-        self.randomize_position()
+        self.position = None
 
     def randomize_position(self, occupied_positions=None):
         """Устанавливает случайное положение яблока."""
@@ -87,22 +96,22 @@ class Apple(GameObject):
         self.position = random.choice(free_positions)
 
     def draw(self):
-        """Рисует яблоко на экране."""
+        """Рисует яблоко."""
         rect = pg.Rect(self.position, (GRID_SIZE, GRID_SIZE))
         pg.draw.rect(screen, self.body_color, rect)
         pg.draw.rect(screen, BORDER_COLOR, rect, 1)
 
 
 class Snake(GameObject):
-    """Класс для змейки, наследуется от GameObject."""
+    """Класс для змейки."""
 
     def __init__(self, position=POSITION, body_color=SNAKE_COLOR):
-        """Инициализирует змейку с позицией и цветом."""
+        """Инициализирует змейку."""
         super().__init__(position, body_color)
         self.reset()
 
     def update_direction(self):
-        """Обновляет направление движения змейки."""
+        """Обновляет направление змейки."""
         if self.next_direction:
             self.direction = self.next_direction
             self.next_direction = None
@@ -116,13 +125,11 @@ class Snake(GameObject):
         new_head = (new_x, new_y)
         self.positions.insert(0, new_head)
         self.last = (
-            self.positions.pop()
-            if len(self.positions) > self.length
-            else None
+            self.positions.pop() if len(self.positions) > self.length else None
         )
 
     def draw(self):
-        """Рисует змейку на экране."""
+        """Рисует змейку."""
         if self.last:
             last_rect = pg.Rect(self.last, (GRID_SIZE, GRID_SIZE))
             pg.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
@@ -137,7 +144,7 @@ class Snake(GameObject):
         pg.draw.rect(screen, BORDER_COLOR, head_rect, 1)
 
     def get_head_position(self):
-        """Возвращает позицию головы змейки."""
+        """Возвращает позицию головы."""
         return self.positions[0]
 
     def reset(self):
@@ -180,7 +187,6 @@ def main():
     running = True
     while running:
         handle_keys(snake)
-
         clock.tick(SPEED)
         snake.update_direction()
         snake.move()
